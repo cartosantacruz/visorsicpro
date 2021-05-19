d7ab1ee138630ca9ef3053abb479966035471f6b
cartosantacruz <77216776+cartosantacruz@users.noreply.github.com>
2021-05-19 13:08:25 -0300

    Add files via upload

---
+++ data/Terminalesdemnibus_1.js
@@ -0,0 +1,23 @@
+var json_Terminalesdemnibus_1 = {
+"type": "FeatureCollection",
+"name": "Terminalesdemnibus_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Localidad": "28 de Noviembre", "Domicilio": "Chubut 450", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -72.224149, -51.600472 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Caleta Olivia", "Domicilio": "Av. De los Inmigrantes  350", "Telefono": "0297-4850887" }, "geometry": { "type": "Point", "coordinates": [ -67.540639, -46.438951 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Cañadón Seco", "Domicilio": null, "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -67.6122702, -46.56066 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Cmte. Piedra Buena", "Domicilio": "Av. Gregorio Ibañez  157", "Telefono": "02962-497116" }, "geometry": { "type": "Point", "coordinates": [ -68.908911, -49.982652 ] } },
+{ "type": "Feature", "properties": { "Localidad": "El Calafate", "Domicilio": "Antoine de Saint Exsupery  87", "Telefono": "02902-491476" }, "geometry": { "type": "Point", "coordinates": [ -72.245209, -50.337396 ] } },
+{ "type": "Feature", "properties": { "Localidad": "El Chaltén", "Domicilio": "Perito Moreno 28", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -72.883114, -49.333765 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Gdor. Gregores", "Domicilio": "Colón S\/N", "Telefono": "02962-491475" }, "geometry": { "type": "Point", "coordinates": [ -70.244936, -48.74905 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Las Heras", "Domicilio": "Ramos Mejía esq. Los Sauces", "Telefono": "0297-4974750" }, "geometry": { "type": "Point", "coordinates": [ -68.935043, -46.537813 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Los Antiguos", "Domicilio": "Av. Tehuelches   157", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -71.624649, -46.543915 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Perito Moreno", "Domicilio": "Cristóbal Colón 1299-1399", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -70.928242, -46.582441 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Pico Truncado", "Domicilio": "Jose Fuchs   152", "Telefono": "0297-4992202" }, "geometry": { "type": "Point", "coordinates": [ -67.947777, -46.797423 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Puerto Deseado", "Domicilio": "Av. San Martín 1401", "Telefono": "0297-4870013" }, "geometry": { "type": "Point", "coordinates": [ -65.884412, -47.746871 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Puerto Santa Cruz", "Domicilio": "Av. Roca y Catamarca", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -68.532202, -50.020789 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Río Gallegos", "Domicilio": "Eva Peron  1451-1499", "Telefono": "02966-442169" }, "geometry": { "type": "Point", "coordinates": [ -69.247503, -51.627681 ] } },
+{ "type": "Feature", "properties": { "Localidad": "Río Turbio", "Domicilio": "Agustin Castillo  130", "Telefono": "02902-421422" }, "geometry": { "type": "Point", "coordinates": [ -72.337815, -51.53752 ] } },
+{ "type": "Feature", "properties": { "Localidad": "San Julián", "Domicilio": "Av. San Martin  1570", "Telefono": null }, "geometry": { "type": "Point", "coordinates": [ -67.735812, -49.304359 ] } }
+]
+}
