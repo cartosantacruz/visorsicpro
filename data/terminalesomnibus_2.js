ad95ee5d784a1d94e38dba9bb4a98e58ec277b0e
cartosantacruz <77216776+cartosantacruz@users.noreply.github.com>
2021-05-21 13:08:36 -0300

    Add files via upload

---
+++ data/terminalesomnibus_2.js
@@ -0,0 +1,23 @@
+var json_terminalesomnibus_2 = {
+"type": "FeatureCollection",
+"name": "terminalesomnibus_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "localidad": "Río Gallegos", "domicilio": "Eva Peron  1451-1499", "telefono": "02966-442169", "latitudy": -51.627681, "longitudx": -69.247503 }, "geometry": { "type": "Point", "coordinates": [ -69.247503, -51.627681 ] } },
+{ "type": "Feature", "properties": { "localidad": "San Julián", "domicilio": "Av. San Martin  1570", "telefono": null, "latitudy": -49.304359, "longitudx": -67.735812 }, "geometry": { "type": "Point", "coordinates": [ -67.735812, -49.304359 ] } },
+{ "type": "Feature", "properties": { "localidad": "El Calafate", "domicilio": "Antoine de Saint Exsupery  87", "telefono": "02902-491476", "latitudy": -50.337396, "longitudx": -72.245209 }, "geometry": { "type": "Point", "coordinates": [ -72.245209, -50.337396 ] } },
+{ "type": "Feature", "properties": { "localidad": "Caleta Olivia", "domicilio": "Av. De los Inmigrantes  350", "telefono": "0297-4850887", "latitudy": -46.438951, "longitudx": -67.540639 }, "geometry": { "type": "Point", "coordinates": [ -67.540639, -46.438951 ] } },
+{ "type": "Feature", "properties": { "localidad": "Pico Truncado", "domicilio": "Jose Fuchs   152", "telefono": "0297-4992202", "latitudy": -46.797423, "longitudx": -67.947777 }, "geometry": { "type": "Point", "coordinates": [ -67.947777, -46.797423 ] } },
+{ "type": "Feature", "properties": { "localidad": "Río Turbio", "domicilio": "Agustin Castillo  130", "telefono": "02902-421422", "latitudy": -51.53752, "longitudx": -72.337815 }, "geometry": { "type": "Point", "coordinates": [ -72.337815, -51.53752 ] } },
+{ "type": "Feature", "properties": { "localidad": "El Chaltén", "domicilio": "Perito Moreno 28", "telefono": null, "latitudy": -49.333765, "longitudx": -72.883114 }, "geometry": { "type": "Point", "coordinates": [ -72.883114, -49.333765 ] } },
+{ "type": "Feature", "properties": { "localidad": "Cañadón Seco", "domicilio": null, "telefono": null, "latitudy": -46.56066, "longitudx": -67.6122702 }, "geometry": { "type": "Point", "coordinates": [ -67.6122702, -46.56066 ] } },
+{ "type": "Feature", "properties": { "localidad": "Cmte. Piedra Buena", "domicilio": "Av. Gregorio Ibañez  157", "telefono": "02962-497116", "latitudy": -49.982652, "longitudx": -68.908911 }, "geometry": { "type": "Point", "coordinates": [ -68.908911, -49.982652 ] } },
+{ "type": "Feature", "properties": { "localidad": "Puerto Santa Cruz", "domicilio": "Av. Roca y Catamarca", "telefono": null, "latitudy": -50.020789, "longitudx": -68.532202 }, "geometry": { "type": "Point", "coordinates": [ -68.532202, -50.020789 ] } },
+{ "type": "Feature", "properties": { "localidad": "Gdor. Gregores", "domicilio": "Colón S\/N", "telefono": "02962-491475", "latitudy": -48.74905, "longitudx": -70.244936 }, "geometry": { "type": "Point", "coordinates": [ -70.244936, -48.74905 ] } },
+{ "type": "Feature", "properties": { "localidad": "28 de Noviembre", "domicilio": "Chubut 450", "telefono": null, "latitudy": -51.600472, "longitudx": -72.224149 }, "geometry": { "type": "Point", "coordinates": [ -72.224149, -51.600472 ] } },
+{ "type": "Feature", "properties": { "localidad": "Las Heras", "domicilio": "Ramos Mejía esq. Los Sauces", "telefono": "0297-4974750", "latitudy": -46.537813, "longitudx": -68.935043 }, "geometry": { "type": "Point", "coordinates": [ -68.935043, -46.537813 ] } },
+{ "type": "Feature", "properties": { "localidad": "Puerto Deseado", "domicilio": "Av. San Martín 1401", "telefono": "0297-4870013", "latitudy": -47.746871, "longitudx": -65.884412 }, "geometry": { "type": "Point", "coordinates": [ -65.884412, -47.746871 ] } },
+{ "type": "Feature", "properties": { "localidad": "Perito Moreno", "domicilio": "Cristóbal Colón 1299-1399", "telefono": null, "latitudy": -46.582441, "longitudx": -70.928242 }, "geometry": { "type": "Point", "coordinates": [ -70.928242, -46.582441 ] } },
+{ "type": "Feature", "properties": { "localidad": "Los Antiguos", "domicilio": "Av. Tehuelches   157", "telefono": null, "latitudy": -46.543915, "longitudx": -71.624649 }, "geometry": { "type": "Point", "coordinates": [ -71.624649, -46.543915 ] } }
+]
+}
