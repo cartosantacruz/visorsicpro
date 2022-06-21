dfe37d052f8aa0d5e3a3004246d34bad93aebb0f
cartosantacruz <77216776+cartosantacruz@users.noreply.github.com>
2022-06-21 13:28:06 -0300

    Delete data directory

--- data/Delegaciones_2.js
+++
@@ -1,9 +0,0 @@
-var json_Delegaciones_2 = {
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "localidad": "Rí­o Gallegos", "domicilio": "Avellaneda N° 801", "latitudx": -69.224647, "longitudy": -51.614378, "telefono": "02966-420189-438728", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -69.224647, -51.614378 ] } },
-{ "type": "Feature", "properties": { "localidad": "El Calafate", "domicilio": "1° de Mayo N° 50", "latitudx": -72.26827, "longitudy": -50.339174, "telefono": "02902-492770", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -72.26827, -50.339174 ] } },
-{ "type": "Feature", "properties": { "localidad": "Caleta Olivia", "domicilio": "Av. Perón N° 10", "latitudx": -67.517113, "longitudy": -46.442137, "telefono": "02966-637093", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -67.517113, -46.442137 ] } }
-]
-}
