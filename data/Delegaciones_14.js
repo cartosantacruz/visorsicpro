74f356ae036204d49ac57ef6349103e4512ba46b
cartosantacruz <77216776+cartosantacruz@users.noreply.github.com>
2022-06-21 13:30:09 -0300

    Add files via upload

---
+++ data/Delegaciones_14.js
@@ -0,0 +1,9 @@
+var json_Delegaciones_14 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "localidad": "Rí­o Gallegos", "domicilio": "Avellaneda N° 801", "latitudx": -69.224647, "longitudy": -51.614378, "telefono": "02966-420189-438728", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -69.224647, -51.614378 ] } },
+{ "type": "Feature", "properties": { "localidad": "El Calafate", "domicilio": "1° de Mayo N° 50", "latitudx": -72.26827, "longitudy": -50.339174, "telefono": "02902-492770", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -72.26827, -50.339174 ] } },
+{ "type": "Feature", "properties": { "localidad": "Caleta Olivia", "domicilio": "Av. Ing. Carlos Lando", "latitudx": -67.495879, "longitudy": -46.469113, "telefono": "02966-637093", "sag": "Ministerio de la Producción, Comercio e Industria", "fdc": "Subsecretaría de Transporte" }, "geometry": { "type": "Point", "coordinates": [ -67.495802025974939, -46.468962273352112 ] } }
+]
+}
---
+++ data/Proyectos_16.js
@@ -0,0 +1,18 @@
+var json_Proyectos_16 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "302857", "nombre_cor": "Fitz Roy", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -67.247396841146355, -47.026347788685698 ] } },
+{ "type": "Feature", "properties": { "id": "302858", "nombre_cor": "Jaramillo", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -67.144724008001404, -47.184028410973625 ] } },
+{ "type": "Feature", "properties": { "id": "302859", "nombre_cor": "Koluel Kaike", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -68.22358090786048, -46.716815393179417 ] } },
+{ "type": "Feature", "properties": { "id": "302861", "nombre_cor": "Pico Truncado", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -67.957851680220145, -46.799420175550352 ] } },
+{ "type": "Feature", "properties": { "id": "302862", "nombre_cor": "Puerto Deseado", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -65.900948726786055, -47.751719408809535 ] } },
+{ "type": "Feature", "properties": { "id": "302867", "nombre_cor": "Río Gallegos", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -69.215864533123707, -51.623463266373925 ] } },
+{ "type": "Feature", "properties": { "id": "302869", "nombre_cor": "28 de Noviembre", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -72.211640393481559, -51.579613896057303 ] } },
+{ "type": "Feature", "properties": { "id": "302870", "nombre_cor": "Yacimientos Río Turbio", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -72.337854905293895, -51.536422639383424 ] } },
+{ "type": "Feature", "properties": { "id": "302872", "nombre_cor": "El Chaltén", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -72.88591141991219, -49.33282465538209 ] } },
+{ "type": "Feature", "properties": { "id": "302873", "nombre_cor": "Tres Lagos", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -71.446746789992019, -49.597910203592534 ] } },
+{ "type": "Feature", "properties": { "id": "302878", "nombre_cor": "Gobernador Gregores", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -70.24851965962398, -48.750897337205295 ] } },
+{ "type": "Feature", "properties": { "id": "302879", "nombre_cor": "Lago Posadas", "Web": "http:\/\/www.sicpro.transporteminpro.com.ar\/" }, "geometry": { "type": "Point", "coordinates": [ -71.742848247281444, -47.565662519175611 ] } }
+]
+}
